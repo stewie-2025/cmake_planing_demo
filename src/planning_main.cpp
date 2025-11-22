9d5cb46b49feedd15dd353df73ae231f6769852d
lianglonglong20241116 <3014890139@qq.com>
2025-11-22 21:23:53 +0800

    version 0.0.1

---
+++ src/planning_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "process.h"
+using std::cout,std::endl;
+
+int main()
+{
+    cout << "planning start-33-003" << endl;
+    Process pro;
+    pro.planProcess();
+    cout << "planning end.-333-003" << endl;
+    
+    return 0;
+}       
