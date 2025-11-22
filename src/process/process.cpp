1d1e07cd99009035ac553ebe3ffc91eb454b1d92
lianglonglong20241116 <3014890139@qq.com>
2025-11-22 20:59:37 +0800

    basic mode

---
+++ src/process/process.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "process.h"
+using std::cout,std::endl;
+
+void Process::planProcess()
+{
+    cout << "this is planing process-22-002" << endl;
+    my_map.mapInfo();
+    cout << "planing success!-222-002" << endl;
+}
